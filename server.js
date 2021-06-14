179cb1232f917bc44fd0859704cdfd660f17a67c
ibrahim-alhamshari <ibrahimalhamshari742@gmail.com>
2021-06-14 15:18:42 +0300

    some chnges

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ const cors = require('cors');
 
 app.use(cors()) // after you initialize your express app instance
 
+require('dotenv').config();
+const port = process.env.port;
+
 // a server endpoint 
 app.get('/', // our endpoint name
  function (req, res) { // callback function of what we should do with our request
   res.send('Hello World') // our endpoint function response
 })
  
-app.listen(3000) // kick start the express server to work
+app.listen(port) // kick start the express server to work
