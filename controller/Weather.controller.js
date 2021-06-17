2ee8d25b21c043db691f6b8716e955a55105aaef
ibrahim-alhamshari <ibrahimalhamshari742@gmail.com>
2021-06-17 23:28:28 +0300

    adding date property

--- controller/Weather.controller.js
+++ controller/Weather.controller.js
@@ -2,44 +2,42 @@ const WEATHER_BIT_KEY = process.env.WEATHER_BIT_KEY;
 const axios = require('axios');
 require('dotenv').config();
 const Forecast = require('../models/Weather.models');
-const Cache =require('../helper/cache');
+const Cache = require('../helper/cache');
 
-const cacheObj= new Cache();
+const cacheObj = new Cache();
 
 const weatherController = (request, res) => {
     let lat = request.query.lat;
     let lon = request.query.lon;
-    let requestKey= `weather-data-${lat}-${lon}`
-// 
-let currentDate = Date.now();
-
+    let requestKey = `weather-data-${lat}-${lon}`
+    // 
+    let currentDate = Date.now();
+    let date;
 
     if (lat && lon) {
-        if(cacheObj[requestKey] && ((Date.now() - cacheObj[requestKey].timeStamp) < 8640000)){
-            console.log('===================');
-            console.log('from the cache object');
-            console.log(currentDate -cacheObj[requestKey].timeStamp);
-            console.log('===================');
+        if (cacheObj[requestKey] && ((currentDate - cacheObj[requestKey].timeStamp) < 86400000)) {
             const results = cacheObj[requestKey]
             res.json(results);
-        }else{
-        const weatherBitUrl = `https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_BIT_KEY}&lat=${lat}&lon=${lon}`;
-        axios.get(weatherBitUrl).then(response => {
-            const responseData = response.data.data.map(object => new Forecast(object));
-            cacheObj[requestKey]= {cachedData:responseData};
-            cacheObj[requestKey].timeStamp= Date.now();
-            const previusDate = cacheObj[requestKey].timeStamp;
-            const cacheVariable= cacheObj[requestKey];
-            console.log(responseData);
-            console.log('=======');
-            console.log(cacheVariable);
+        } else {
+            const weatherBitUrl = `https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_BIT_KEY}&lat=${lat}&lon=${lon}`;
+            axios.get(weatherBitUrl).then(response => {
+                const responseData = response.data.data.map(object => new Forecast(object));
+                
+                cacheObj[requestKey] = { cachedData: responseData };
+                const previusDate = Date.now();
+                cacheObj[requestKey].timeStamp = previusDate;
+                const cacheVariable = cacheObj[requestKey];
 
-            res.json(cacheVariable);
+                date = new Date(1546108200 * 1000);
+                cacheObj[requestKey].date = date;
+                console.log(cacheObj[requestKey].date);
+                
+                res.json(cacheVariable);
 
-        }).catch(error => {
-            res.send(error.message);
-        })
-    }
+            }).catch(error => {
+                res.send(error.message);
+            })
+        }
 
     } else {
         res.send('please provide the proper lat and lon')
