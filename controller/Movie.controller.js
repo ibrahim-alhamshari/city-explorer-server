fde4f78007e159ab9535e8ae5699a01af5cff405
ibrahim-alhamshari <ibrahimalhamshari742@gmail.com>
2021-06-17 16:47:54 +0300

    making the cache

--- controller/Movie.controller.js
+++ controller/Movie.controller.js
@@ -1,38 +1,42 @@
 const MovieClass = require('../models/Movie.models');
 const key = process.env.MOVIE_API_KEY;
 const axios = require('axios');
+const Cache = require('../helper/cache');
+
+const cacheObj = new Cache();
 
 const movieController = (req, res) => {
-    let cityName = req.query.cityName;
+  let cityName = req.query.cityName;
+let requestKey =cityName;
+  if (cityName) {
+    if (cacheObj[requestKey]) {
+      const results = cacheObj[requestKey];
+      console.log(results);
+      res.send(results);
 
+    } else {
+      const moviesGetting = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${cityName}`;
 
-    console.log(cityName);
-    console.log(req.query);
-
-    if (cityName) {
-        const moviesGetting = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${cityName}`;
-
-        axios.get(moviesGetting)
-            .then(object => {
-                const respnsedData = object.data.results.filter(element => {
-                    console.log(element.poster_path);
-                    if (element.poster_path == null) {
-                  console.log('true');
-            }else{
-                console.log('else');      
-                  return new MovieClass(element);
-            }
-         }
-        
-              )
-            res.send(respnsedData)
-            })
-  .catch ((error) => {
-    res.send(error);
-})
-  }else {
+      axios.get(moviesGetting)
+        .then(object => {
+          const respnsedData = object.data.results.filter(element => {
+            if (element.poster_path == null) {
+              console.log('true');
+            } else {
+              return new MovieClass(element);
+            }})
+            cacheObj[requestKey]= {data:respnsedData};
+            cacheObj[requestKey].timeStamp= Date.now();
+            console.log(cacheObj[requestKey]);
+          res.send(respnsedData)
+        })
+        .catch((error) => {
+          res.send(error);
+        })
+    }
+  } else {
     res.send('Please enter the city name. Use this as the same: (?cityName=) then ; the name of the country');
-}
   }
+}
 
 module.exports = movieController;
--- controller/Weather.controller.js
+++ controller/Weather.controller.js
@@ -2,21 +2,39 @@ const WEATHER_BIT_KEY = process.env.WEATHER_BIT_KEY;
 const axios = require('axios');
 require('dotenv').config();
 const Forecast = require('../models/Weather.models');
+const Cache =require('../helper/cache');
+
+const cacheObj= new Cache();
 
 const weatherController = (request, res) => {
     console.log(request.query);
     let lat = request.query.lat;
     let lon = request.query.lon;
+    let requestKey= `weather-data-${lat}-${lon}`
+// 
+let currentDate = Date.now();
+
 
     if (lat && lon) {
+        if(cacheObj[requestKey] && ((Date.now() - cacheObj[requestKey].timeStamp) < 8640000)){
+            console.log('===================');
+            console.log('from the cache object');
+            console.log(currentDate -cacheObj[requestKey].timeStamp);
+            console.log('===================');
+            const results = cacheObj[requestKey]
+            res.json(results);
+        }else{
         const weatherBitUrl = `https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_BIT_KEY}&lat=${lat}&lon=${lon}`;
-console.log(weatherBitUrl);
         axios.get(weatherBitUrl).then(response => {
-            const responseData = response.data.data.map(object => new Forecast(object))
+            const responseData = response.data.data.map(object => new Forecast(object));
+            cacheObj[requestKey]= {data:responseData};
+            cacheObj[requestKey].timeStamp= Date.now();
+            
             res.json(responseData);
-           }).catch(error => {
+        }).catch(error => {
             res.send(error.message);
         })
+    }
 
     } else {
         res.send('please provide the proper lat and lon')
