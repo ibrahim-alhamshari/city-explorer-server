2f47a3ad3aa4b47b0a80b603b42e829d4a2f73e8
ibrahim-alhamshari <ibrahimalhamshari742@gmail.com>
2021-07-03 10:46:26 +0300

    Adding some description and explain the concatination

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const express = require('express') // require the express package
+const express = require('express') //Our express Framework. require the express package (npm install express)
 const app = express() // initialize your express app instance
+const cors = require('cors'); // cors "Used to allow resource sharing between the frontend and the backend" (npm install cors)
+app.use(cors()); // after you initialize your express app instance
+require('dotenv').config(); //dotenv "A package to help us read our env variables" (npm install dotenv)
+const axios = require('axios'); // superagent "A package used to send requests over the internet to other servers or APIs" (npm install axios)
+
+const PORT = process.env.PORT;
+
 const weatherData = require('./data/weather.json');
-const axios = require('axios');
-const cors = require('cors');
-require('dotenv').config();
 const { request, response } = require('express');
 const WeatherController = require('./controller/Weather.controller');
 const indexController = require('./controller/index.controller');
 const movieController =require('./controller/Movie.controller');
-const PORT = process.env.PORT;
-app.use(cors()); // after you initialize your express app instance
 
 
 app.get('/', indexController);
@@ -20,8 +22,7 @@ app.get('/movie' , movieController)
 
 app.listen(PORT)
 //   ,()=>{
-//   console.log('this like PORT' , PORT)
-// }) // kick start the express server to work
+    //   console.log('this like PORT' , PORT)
+    // }) // kick start the express server to work
 
-
-
+    
